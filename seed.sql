e09df68919f8a45b976571885e23e1c3edc4743c
HannahStarcevich <starcevichh@gmail.com>
2021-01-21 19:10:21 -0800

    updates to the schema, seed and inquirer prompts

--- seed.sql
+++ seed.sql
@@ -3,15 +3,22 @@ USE employee_tracker_db;
 
 -- department
 INSERT INTO department (name)
-VALUES ("bakery")
+VALUES ("HR"), ("Sales"), ("IT"), ("Operations");
 
 -- role
 INSERT INTO role (title, salary, department_id)
-VALUES ("head chef", "500000", 1)
+VALUES ("Recruiter", 70000, 1), ("Technical Recruiter", 80000, 1), ("Seller", 95000, 2),("Software Engineer", 120000, 3),("Operations Lead", 90000, 4),("Boss", 140000, 3);
 
 -- employees
 INSERT INTO employees (first_name, last_name, role_id)
-VALUES ("hannah", "starcevich", 1)
+VALUES ("head", "hancho", 6);
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("kevin", "lyons", 1, 1)
-
+VALUES ("hannah", "banana", 4, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("joe", "shmoe", 1, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("jack", "black", 2, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("jessie", "bessie", 3, 1);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("mike", "ike", 5, 1);
--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
     port: 3306,
 
-    // Your username
     user: "root",
 
-    // Your password
     password: "password",
     database: "employee_tracker_db"
 });
@@ -20,3 +17,116 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId + "\n");
     start()
 });
+
+function start() {
+    inquirer.prompt({
+            name: "start",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.start) {
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+
+                case "View All Employees by Department":
+                    viewEmployeesDept();
+                    break;
+
+                case "View All Employees by Manager":
+                    viewEmployeesManager();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;
+
+                    // case "exit":
+                    //     connection.end();
+                    //     break;
+            }
+        })
+}
+
+
+function viewEmployees() {
+    console.log("fetching employee data...")
+    var query = "SELECT employees.id, first_name, last_name, department.name AS department, role.title AS role, salary, manager_id FROM employees JOIN role ON employees.role_id = role.id JOIN department ON role.department_id = department.id"
+
+    connection.query(query, function (err, res) {
+        if (err) {
+            throw err
+        } else {
+            console.table(res)
+            start()
+        }
+    })
+}
+
+function viewEmployeesDept() {
+    inquirer.prompt({
+        name: "employeeDept",
+        type: "list",
+        message: "Select a department",
+        choices: [
+            "HR",
+            "Sales",
+            "IT",
+            "Operations",
+        ]
+    }).then(function (answer) {
+        console.log("fetching employee department data...")
+        var query = "SELECT employees.id, first_name, last_name, department.name AS department FROM employees JOIN role ON employees.role_id = role.id JOIN department ON role.department_id = department.id WHERE ?"
+
+        connection.query(query, {
+            employeeDept: answer.employeeDept
+        }, function (err, res) {
+            if (err) {
+                throw err
+            } else {
+                for (var i = 0; i < res.length; i++) {
+                    console.table(res)
+                }
+                start();
+            }
+        });
+    })
+
+}
+
+// function viewEmployeesManager() {}
+
+// function addEmployee() {}
+
+// function removeEmployee() {}
+
+// function updateEmployeeRole() {}
+
+// function updateEmployeeManager() {}
+
+
+
+
+// value is the id 
